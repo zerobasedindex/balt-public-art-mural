5bc6915e89255a07f79096a36bdb07e40387e4b9
Cody Henthorne <cody.zbi@gmail.com>
2017-01-16 20:19:03 -0500

    Basic map activity

--- app/src/main/java/com/gdgbaltimore/bmoreart/HomeActivity.java
+++ app/src/main/java/com/gdgbaltimore/bmoreart/HomeActivity.java
@@ -3,11 +3,25 @@ package com.gdgbaltimore.bmoreart;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HomeActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class HomeActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
