8c8c9da4d84dcd1e419f7f9ff63cffc5412cef37
Domenic Thomas <domenicthomas@gmail.com>
2017-02-16 21:35:19 -0500

    1 down, 2 to go

--- app/src/main/java/com/gdgbaltimore/bmoreart/HomeActivity.java
+++ app/src/main/java/com/gdgbaltimore/bmoreart/HomeActivity.java
@@ -3,9 +3,12 @@ package com.gdgbaltimore.bmoreart;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class HomeActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -23,8 +26,10 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         // Setup camera to point to baltimore
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(39.2904, -76.6122), 12));
 
         // Add at least 3 pins, maybe OrderUp HQ, Goucher College, and pick a restaurant
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(39.2818289, -76.584183)));
 
         // Draw a polyline around the Baltimore city boundry
         // You'll have to google to get lat/lon points for that boundry
