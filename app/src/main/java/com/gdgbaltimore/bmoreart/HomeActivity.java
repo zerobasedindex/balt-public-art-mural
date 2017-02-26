8a8862b5a2d73f83b72c92acf96a4eb9422281cc
Domenic Thomas <domenicthomas@gmail.com>
2017-02-26 14:58:56 -0500

    Added  markers for Goucher college and Baltimore museum of art Created Polyline boundary

--- app/src/main/java/com/gdgbaltimore/bmoreart/HomeActivity.java
+++ app/src/main/java/com/gdgbaltimore/bmoreart/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.gdgbaltimore.bmoreart;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +10,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import static com.gdgbaltimore.bmoreart.R.id.map;
 
 public class HomeActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -18,7 +23,7 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_home);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+                .findFragmentById(map);
 
         mapFragment.getMapAsync(this);
     }
@@ -26,12 +31,24 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         // Setup camera to point to baltimore
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(39.2904, -76.6122), 12));
+        // Changed zoom to 11 from 12(Dom)
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(39.2904, -76.6122), 11));
 
-        // Add at least 3 pins, maybe OrderUp HQ, Goucher College, and pick a restaurant
+        // Add at least 3 pins, maybe OrderUp HQ, Goucher College, pick a restaurant
+        //Chose to add Baltimore Museum of Art(Dom)
         googleMap.addMarker(new MarkerOptions().position(new LatLng(39.2818289, -76.584183)));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(39.408765, -76.596545)));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(39.3266504, -76.6048989)));
+
 
         // Draw a polyline around the Baltimore city boundry
         // You'll have to google to get lat/lon points for that boundry
+
+        Polyline line = googleMap.addPolyline(new PolylineOptions().add(new LatLng(39.372264, -76.711529), new LatLng(39.372264, -76.529649), new LatLng(39.207672, -76.532966),
+                new LatLng(39.197325, -76.550081), new LatLng(39.207578, -76.583704), new LatLng(39.234661, -76.611865), new LatLng(39.277896, -76.711035), new LatLng(39.372264, -76.711529))
+                .width(5)
+                .color(Color.RED));
+
     }
+
 }
